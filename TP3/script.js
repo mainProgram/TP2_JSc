edf8ec47f03a5e6affc2fe3bba304a1bbc53295f
mainProgram <fazeyfadiop@gmail.com>
2022-03-19 12:04:50 +0000

    TP3

--- TP3/script.js
+++ TP3/script.js
@@ -1,26 +1,141 @@
-const gallery = document.getElementById("gallery")
-const right = document.getElementById("right")
-const left = document.getElementById("left")
-const img = document.getElementById("img");
+//--------------------------------------------------------------------------------------------------DECLARATIONS
 const zoom_result = document.querySelector(".img-zoom-result")
-const tab = ["img/img1.jpg", "img/img2.jpg", "img/img3.jpg", "img/img4.jpg"]
+const slides = document.querySelector(".slides")
+const slider = document.querySelector(".slider")
+const left = document.querySelector("#left")
+const right = document.querySelector("#right")
+const length = 11
 
+//--------------------------------------------------------------------------------------------------HTML STRUCTURATION
+//INPUTS RADIO
+for (let i = 1; i <= length; i++) {
+    input = document.createElement("input")
+    input.setAttribute("type", "radio")
+    input.setAttribute("name", "radio")
+    input.setAttribute("id", "radio"+i)
+    if(i==1)
+        input.setAttribute("checked", true)
 
-var s = function setImage(image){
-    img.src = image
+    slides.appendChild(input)
 }
 
-function setImagePer5Seconds(){
-    tab.forEach((element) => {
-        setTimeout(s, 3000, element)
-    });
+//IMAGES
+for (let i = 1; i <= length; i++) {
+    div = document.createElement("div")
+    div.classList.add("slide")
+    if(i==1)
+        div.classList.add("first")
+
+    img = document.createElement("img")
+    img.setAttribute("src", "img/img"+i+".jpg")
+    img.setAttribute("alt", "flowers")
+    img.setAttribute("id", "img"+i)
+
+    div.appendChild(img)
+    slides.appendChild(div)
 }
 
-window.onload = ()=>{
-    setImagePer5Seconds()
-}
+const navigationAuto = document.createElement("div")
+navigationAuto.setAttribute("class", "navigation-auto")
+for (let i = 1; i <= length; i++) {
+    //DIV AUTO
+    div = document.createElement("div")
+    div.classList.add("auto"+i)
+
+    navigationAuto.appendChild(div)
+    slides.appendChild(navigationAuto)
+}  
 
 
+const navigationManual = document.createElement("div")
+navigationManual.setAttribute("class", "navigation-manual")
+for (let i = 1; i <= length; i++) {
+    //DIV MANUAL
+    label = document.createElement("label")
+    label.classList.add("manual")
+    label.setAttribute("for", "radio"+i)
+
+    navigationManual.appendChild(label)
+    slides.appendChild(navigationManual)
+}  
+
+//--------------------------------------------------------------------------------------------------EVENTS
+//EVERY 5 SECONDS SHOW AN IMG
+var compteur = 1;
+setInterval(function(){
+    document.getElementById("radio" + compteur).checked = true;
+    
+    compteur++;
+
+    if(compteur > length){
+        compteur = 1
+        right.style.display = "none"
+    }
+    else if(compteur - 1  == 1){
+        left.style.display = "none"
+        right.style.display = "block"
+    }
+    else{
+        left.style.display = "block"
+        right.style.display = "block"
+    }
+      
+}, 3000);
+
+//EVERY 5 SECONDS ZOOM FOT ALL IMAGES
+const allImages = slides.querySelectorAll("img");
+allImages.forEach((image, i) => image.addEventListener("mousemove", () => {
+        zoom_result.style.display = "block";
+        imageZoom("img"+(i+1), "myresult");
+    })
+)
+
+//REMOVE THE ZOOM RESULT
+window.addEventListener("mouseover", (e)=>{
+    if(e.target == document.body)
+        zoom_result.style.display = "none"
+    else
+        return false;
+    console.log(e.target)
+})
+
+//WHEN I CLICK ON A RADIO STAY THERE N UPDATE COMPTEUR
+const labels = document.querySelectorAll("label")
+labels.forEach((label, i) => label.addEventListener("click", () => {
+        document.getElementById("radio"+(i+1)).checked = true;
+        compteur = i + 1
+        // console.dir(label.control.checked)
+    })
+)
+
+var id = 0
+// WHEN I CLICK ON THE RIGHT BUTTON
+right.addEventListener("click", ()=>{
+    labels.forEach((label, i) => {
+        if(label.control.checked == true){
+            id = i+2
+            return false
+        }
+    })
+    suivant = document.getElementById("radio"+id)
+    suivant.checked = true
+    compteur = id
+})
+
+// WHEN I CLICK ON THE RIGHT BUTTON
+left.addEventListener("click", ()=>{
+    labels.forEach((label, i) => {
+        if(label.control.checked == true){
+            id = i
+            return false
+        }
+    })
+    precedent = document.getElementById("radio"+id)
+    precedent.checked = true
+    compteur = id
+})
+
+//--------------------------------------------------------------------------------------------------FUNCTIONS
 function imageZoom(imgID, resultID) {
     var img, lens, result, cx, cy;
     img = document.getElementById(imgID);
@@ -78,17 +193,15 @@ function imageZoom(imgID, resultID) {
     }
 }
 
+// function checked(element) {
+//     return element.checked
+// }
 
-img.addEventListener("mousemove", ()=>{
-    zoom_result.style.display = "block"
-    imageZoom("img", "myresult");
-})
+// function isOneAtLeastChecked(array) {
+//     newArray = array.filter(checked)
+//     if (newArray.length == 0)
+//         return false
+//     return true
+// }
 
-window.addEventListener("mouseover", (e)=>{
-    if(e.target == gallery)
-        zoom_result.style.display = "none"
-    else
-        return false;
-    console.log(e.target)
-})
 
