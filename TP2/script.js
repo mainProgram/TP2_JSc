dfe6687fe1b5806cdd759f2338c99afaccf8af06
mainProgram <fazeyfadiop@gmail.com>
2022-03-21 08:10:35 +0000

    TP2

--- TP2/script.js
+++ TP2/script.js
@@ -5,7 +5,6 @@ const sidebar = document.querySelector(".sidebar")
 const wrapper = document.querySelector(".wrapper")
 const sidebarBtn = document.querySelector(".sidebar-btn")
 const sidebarMenu = document.querySelector(".sidebar-menu")
-const AllMenuBtn = document.querySelectorAll(".menu-btn")
 const AllSubMenu = document.querySelectorAll(".sub-menu")
 const mainContainer = document.querySelector(".main-container")
 
@@ -17,36 +16,194 @@ const tabObjets = [
             "href": "#",
             "class": "menu-btn",
         },
+        firstIcon : "fas fa-search",
+        span: "<input type='text'>",
+        small:{
+            "text": "",
+            "class": ""
+        },
+        secondIcon: "",
+        div:{
+            "class": "",
+            "a": "#",
+            "i": "",
+            "span": ""
+        }
+    },
+    {
+        id: "dashboard",
+        class: "item",
+        a: {
+            "href": "#dashboard",
+            "class": "menu-btn",
+        },
         firstIcon : "fas fa-tachometer-alt",
         span: "Dashboard",
         small:{
-            "text": "New",
-            "class": "red"
+            "text": "",
+            "class": ""
         },
-        secondIcon: "fas fa-chevron-down drop-down",
+        secondIcon: "fas fa-angle-left",
         div:{
             "class": "sub-menu",
             "a": "#",
             "i": "far fa-circle",
             "span": "text"
         }
+    },
+    {
+        id: "widgets",
+        class: "item",
+        a: {
+            "href": "#widgets",
+            "class": "menu-btn",
+        },
+        firstIcon : "fas fa-th",
+        span: "Widgets",
+        small:{
+            "text": "New",
+            "class": "red"
+        },
+        secondIcon: "",
+        div:{
+            "class": "",
+            "a": "",
+            "i": "",
+            "span": ""
+        }
+    },
+    {
+        id: "layout",
+        class: "item",
+        a: {
+            "href": "#layout",
+            "class": "menu-btn",
+        },
+        firstIcon : "far fa-copy",
+        span: "Layout Options",
+        small:{
+            "text": "6",
+            "class": "blue"
+        },
+        secondIcon: "fas fa-angle-left",
+        div:{
+            "class": "sub-menu",
+            "a": "#",
+            "i": "far fa-circle",
+            "span": "text"
+        }
+    },
+    {
+        id: "charts",
+        class: "item",
+        a: {
+            "href": "#charts",
+            "class": "menu-btn",
+        },
+        firstIcon : "fas fa-chart-pie",
+        span: "Charts",
+        small:{
+            "text": "",
+            "class": ""
+        },
+        secondIcon: "fas fa-angle-left",
+        div:{
+            "class": "sub-menu",
+            "a": "#",
+            "i": "far fa-circle",
+            "span": "text"
+        }
+    },
+    {
+        id: "ui",
+        class: "item",
+        a: {
+            "href": "#ui",
+            "class": "menu-btn",
+        },
+        firstIcon : "fas fa-tree",
+        span: "UI Elements",
+        small:{
+            "text": "",
+            "class": ""
+        },
+        secondIcon: "fas fa-angle-left",
+        div:{
+            "class": "sub-menu",
+            "a": "#",
+            "i": "far fa-circle",
+            "span": "text"
+        }
+    },
+    {
+        id: "forms",
+        class: "item",
+        a: {
+            "href": "#forms",
+            "class": "menu-btn",
+        },
+        firstIcon : "fas fa-edit",
+        span: "Forms",
+        small:{
+            "text": "",
+            "class": ""
+        },
+        secondIcon: "fas fa-angle-left",
+        div:{
+            "class": "sub-menu",
+            "a": "#",
+            "i": "far fa-circle",
+            "span": "text"
+        }
+    },
+    {
+        id: "tables",
+        class: "item",
+        a: {
+            "href": "#tables",
+            "class": "menu-btn",
+        },
+        firstIcon : "fas fa-border-all",
+        span: "Tables",
+        small:{
+            "text": "",
+            "class": ""
+        },
+        secondIcon: "fas fa-angle-left",
+        div:{
+            "class": "sub-menu",
+            "a": "#",
+            "i": "far fa-circle",
+            "span": "text"
+        }
+    },
+    {
+        id: "elements",
+        class: "item",
+        a: {
+            "href": "#elements",
+            "class": "menu-btn",
+        },
+        firstIcon : "",
+        span: "Elements",
+        small:{
+            "text": "",
+            "class": ""
+        },
+        secondIcon: "",
+        div:{
+            "class": "",
+            "a": "",
+            "i": "",
+            "span": ""
+        }
     }
 ]
-{/* <li class="item" id="layout">
-    <a href="#layout" class="menu-btn">
-        <i class="far fa-copy"></i><span>Layout Options <small class="blue">6</small><i class="fas fa-chevron-down drop-down"></i></span>
-    </a>
-    <div class="sub-menu">
-        <a href="#"><i class="far fa-circle"></i><span>text</span></a>
-        <a href="#"><i class="far fa-circle"></i><span>text</span></a>
-        <a href="#"><i class="far fa-circle"></i><span>text</span></a>
-    </div>
-</li> */}
-
+//--------------------------------------------------------------------------------------------------HTML STRUCTURATION
 tabObjets.forEach(element => {
     li = document.createElement("li")
-    li.setAttribute("id", element.id)
     li.setAttribute("class", element.class)
+    li.setAttribute("id", element.id)
 
     a = document.createElement("a")
     a.setAttribute("href", element.a.href)
@@ -69,8 +226,6 @@ tabObjets.forEach(element => {
     span.appendChild(small)
     span.appendChild(i2)
     a.appendChild(span)
-
-
 
     var div = document.createElement("div")
     div.setAttribute("class", element.div.class)
@@ -98,21 +253,25 @@ tabObjets.forEach(element => {
 });
 
 
-//--------------------------------------------------------------------------------------------------HTML STRUCTURATION
-
-
-
 //--------------------------------------------------------------------------------------------------FUNCTIONS
 
 
-
-
 //--------------------------------------------------------------------------------------------------EVENTS
+// LES FLECHES
+const drop_down_arrows = document.querySelectorAll(".fa-angle-left")
+drop_down_arrows.forEach((element) => element.addEventListener("click", ()=>{
+    element.classList.toggle("left")
+}))
+
+
+
 // LE BOUTON BURGER
 sidebarBtn.addEventListener("click", ()=>{
     wrapper.classList.toggle("collapse")
 })
 
+
+const AllMenuBtn = document.querySelectorAll(".menu-btn")
 // HOVER SUR LES ICONES DU SIDEBAR
 AllMenuBtn.forEach((element) => element.addEventListener("mouseover", ()=>{
         wrapper.classList.remove("collapse")
